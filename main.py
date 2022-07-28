0263477f10ff01181fae9333e830b2080b820148
Alex <farego@yandex.ru>
2022-07-28 13:11:40 +0300

    Feature 'best_hero_from_all_heroes' is realised

--- main.py
+++ main.py
@@ -6,31 +6,39 @@ heroes_names = ('Hulk', 'Captain America', 'Thanos')
 power_stats_compare_param = 'intelligence'
 
 
-def best_hero_by_power_stats(hero_names, compare_param):
-    def get_id_by_name(name, all_heroes_df):
-        hero = all_heroes_df[all_heroes_df['name'] == name]
-        try:
-            res = int(hero['id'])
-        except TypeError:
-            res = -1
-        return res
+def best_hero_by_power_stats(compare_param, hero_names=None):
+    """Сравнение нескольких заданных (или всех) героев по любому доступному показателю из категории 'powerstats'"""
+
+    search_column = 'powerstats'
 
     super_hero = SuperHero()
     heroes = super_hero.get_all_heroes()
-    all_heroes = pd.DataFrame(heroes)
-    heroes_ids = {name: get_id_by_name(name, all_heroes) for name in hero_names}
-    pprint(heroes_ids)
-    heroes_power_stats = {hero_id: super_hero.get_hero_power_stats_by_id(hero_id)
-                          for hero_id in heroes_ids.values() if hero_id > 0}
+
+    # Для ускорения поиска информации по полученным 'json' данным, создаем структуру данных 'pandas DataFrame'
+    all_heroes_df = pd.DataFrame(heroes)
+
+    # Получаем список индексов строк (выборку) нужных нам героев в таблице pandas (не путать с показателем 'id' героя !!!)
+    hero_indexes_for_search = all_heroes_df[all_heroes_df['name'].isin(heroes_names)].index if hero_names \
+        else range(all_heroes_df.shape[0])
+    # Создаем новую структуру данных (словарь) с нужными нам данными героев {id: powerstats} для их сравнения
+    # TODO 1: в принципе, можно не создавать новую структуру данных 'heroes_power_stats', а работать с
+    #  уже имеющимся у нас pandas датафреймом 'all_heroes_df', но для этого нужно немного заморочиться
+    #  и распаковать вложенные составные 'json' данные 'powerstats' в отдельные колонки уже имеющегося датафрейма.
+    #  Попробую реализовать это позднее :)
+    heroes_power_stats = {all_heroes_df.loc[hero_ind, 'id']: heroes[hero_ind].get(search_column)
+                          for hero_ind in hero_indexes_for_search}
+    print(f'Супергерои для сравнения по показателю "{compare_param}" в формате {{id: {{powerstats}}}}:')
     pprint(heroes_power_stats)
+
+    # Находим лучшего супергероя из сформированной ранее выборки
     if len(heroes_power_stats) > 0 and compare_param in list(heroes_power_stats.values())[0]:
         best_hero_id = max(heroes_power_stats, key=lambda itm: heroes_power_stats[itm].get(compare_param))
-        return best_hero_id, all_heroes[all_heroes['id'] == best_hero_id]['name'].values[0], heroes_power_stats[best_hero_id][compare_param]
+        return best_hero_id, all_heroes_df[all_heroes_df['id'] == best_hero_id]['name'].values[0], heroes_power_stats[best_hero_id][compare_param]
 
 
 if __name__ == '__main__':
     try:
-        h_id, hero_name, param = best_hero_by_power_stats(heroes_names, power_stats_compare_param)
+        h_id, hero_name, param = best_hero_by_power_stats(power_stats_compare_param, hero_names=heroes_names)
         print(f'Лучший по показателю "{power_stats_compare_param}": {hero_name}, '
               f'id = {h_id}, {power_stats_compare_param} = {param}')
     except TypeError:
--- superhero/superhero_api.py
+++ superhero/superhero_api.py
@@ -5,10 +5,8 @@ class SuperHero:
     base_url = 'https://cdn.jsdelivr.net/gh/akabab/superhero-api@0.3.0/api/'
 
     def get_all_heroes(self):
+        """Получить через API сайта список всех супергероев в формате 'json'"""
+
         get_all_url = self.base_url + 'all.json'
         resp = requests.get(get_all_url)
         return resp.json()
-
-    def get_hero_power_stats_by_id(self, hero_id):
-        get_id_url = self.base_url + 'powerstats/' + str(hero_id) + '.json'
-        return requests.get(get_id_url).json()
