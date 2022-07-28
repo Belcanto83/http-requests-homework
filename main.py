593e1f87aefe45e9a9dc97fd979018baea819ded
Alex <farego@yandex.ru>
2022-07-28 16:21:10 +0300

    Task_2 done

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
 from superhero.superhero_api import SuperHero
+from yandex.disk.yandex_disk_api import YandexDisk
 import pandas as pd
+import json
 from pprint import pprint
 
+# Документация по API сайта с супергероями для Задачи №1:  https://akabab.github.io/superhero-api/api/
 heroes_names = ('Hulk', 'Captain America', 'Thanos')
 power_stats_compare_param = 'intelligence'
+
+# Символ разделения должен быть '/'
+path_to_file = 'files_for_upload/test.txt'
 
 
 def best_hero_by_power_stats(compare_param, hero_names=None):
@@ -36,10 +42,27 @@ def best_hero_by_power_stats(compare_param, hero_names=None):
         return best_hero_id, all_heroes_df[all_heroes_df['id'] == best_hero_id]['name'].values[0], heroes_power_stats[best_hero_id][compare_param]
 
 
+def upload_file_to_yandex_disk(file_path: str):
+    with open('info_not_for_git/Ya_D.json') as file:
+        data = json.load(file)
+    token = data['token']
+
+    ya_disk = YandexDisk(token=token)
+    ya_disk.upload_file_to_disk(f'API_python_uploads/{file_path.split("/")[-1]}', file_path)
+    # ya_disk.download_file_from_disk("shinserv/20181106_115148.jpg", "downloaded_file.jpg")
+
+
 if __name__ == '__main__':
+    # Задача №1. Поиск лучшего супергероя
+    print('Задача №1. Поиск лучшего супергероя')
     try:
         h_id, hero_name, param = best_hero_by_power_stats(power_stats_compare_param, hero_names=heroes_names)
         print(f'Лучший по показателю "{power_stats_compare_param}": {hero_name}, '
               f'id = {h_id}, {power_stats_compare_param} = {param}')
     except TypeError:
         print(f'Показатель "{power_stats_compare_param}" не найден! Пожалуйста, проверьте название показателя.')
+    print()
+
+    # Задача №2. Загрузка файла с локального компьютера на Яндекс.Диск
+    print('Задача №2. Загрузка файла с локального компьютера на Яндекс.Диск')
+    upload_file_to_yandex_disk(file_path=path_to_file)
